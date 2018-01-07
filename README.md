edde88499e5e6444161071b1fb019394573eb55f
Tommaso Costantini <info@tommasocostantini.it>
2018-01-07 19:52:51 +0100

    Fixed some code

--- README.md
+++ README.md
@@ -10,31 +10,40 @@ Simple Gulp environment for Wordpress!
 ## Get started
 Clone this repo on your folder. Run inside of it:
 
-	npm install
+`npm install`
 
 or if you prefer Yarn
 
-	yarn install
+`yarn install`
 
 ## Installing a package
 To install a dependency in Wordulp just run
 
-	bower install <package-name> --save
+`bower install <package-name> --save`
 
 or
 
-	yarn add <package-name>
+`yarn add <package-name>`
 
 then you need to run
 
-	gulp inject
+`gulp inject`
 
 and launch the webserver with
 
-	gulp serve
+`gulp serve`
 
 you will now have your dependencies in vendor.js and/or vendor.css
 
+## Move "assets" folder
+For correct operation, move the folder inside the folder of your custom theme.
+The correct path is:
+
+`wp-content/themes/name-custom-theme/assets`
+
+## Change the name of the custom theme
+In the gulpfile change `name-theme` with the name of your custom theme
+
 ## Build for production
-When done with coding remember not to upload "node_modules", "bower_components" and "themes/name-theme/assets" folders on your FTP
+When done with coding remember not to upload "node_modules", "bower_components" and "wp-content/themes/name-custom-theme/assets" folders on your FTP
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
-// Create Variables
+/*
+|--------------------------------------------------------------------------
+| Gulp Imports
+|--------------------------------------------------------------------------
+|
+| Importing gulp variables so we can use both later
+| for configuration settings
+|
+*/
+
 var gulp                = require('gulp');
 var $                   = require('gulp-load-plugins')();
 var runSequence         = require('run-sequence');
 var mainBowerFiles      = require('main-bower-files');
 var browserSync         = require("browser-sync").create();
 
-// Task for SASS files.
+/*
+|--------------------------------------------------------------------------
+| Sass Task
+|--------------------------------------------------------------------------
+|
+| Compile SCSS into assets/scss - Autoprefix it - Minify it
+| Push it into name-theme/css
+|
+*/
+
 gulp.task('sass', function () {
   return gulp.src('wp-content/themes/name-theme/assets/scss/**/*.scss')
   .pipe($.sass().on('error', $.sass.logError))
@@ -18,12 +36,30 @@ gulp.task('sass', function () {
   .pipe(browserSync.stream());
 });
 
+/*
+|--------------------------------------------------------------------------
+| Php Task
+|--------------------------------------------------------------------------
+|
+| Compile php
+|
+*/
+
 gulp.task('php', function() {
   return gulp.src('wp-content/themes/name-theme/**/*.php')
   .pipe(browserSync.stream());
 })
 
-// Task for JS files.
+/*
+|--------------------------------------------------------------------------
+| JS Task
+|--------------------------------------------------------------------------
+|
+| Minify JS 
+| Push it into name-theme/js
+|
+*/
+
 gulp.task('js', function() {
   return gulp.src('wp-content/themes/name-theme/assets/js/**/*.js')
   .pipe($.uglify())
@@ -31,13 +67,29 @@ gulp.task('js', function() {
   .pipe(browserSync.stream());
 });
 
-// Task for BOWER files. 
+/*
+|--------------------------------------------------------------------------
+| Bower Task
+|--------------------------------------------------------------------------
+|
+| Take main files from bower-components - Push them into vendor
+|
+*/
+
 gulp.task('bower', function() {
     return gulp.src(mainBowerFiles(), {base: 'bower_components'})
         .pipe(gulp.dest('wp-content/themes/name-theme/vendor'));
 });
 
-// Task for VENDOR JS files. 
+/*
+|--------------------------------------------------------------------------
+| Vendor JS Task
+|--------------------------------------------------------------------------
+|
+| Take .js files into vendor - Minify them - Push them into name-theme/js
+|
+*/
+
 gulp.task('vendor-js', () => {
   return gulp.src(['wp-content/themes/name-theme/vendor/jquery/**/*.js', 'wp-content/themes/name-theme/vendor/**/*.js'])
       .pipe($.concat('vendor.js'))
@@ -45,7 +97,15 @@ gulp.task('vendor-js', () => {
       .pipe(gulp.dest('wp-content/themes/name-theme/js'))
 });
 
-// Task for VENDOR CSS files.
+/*
+|--------------------------------------------------------------------------
+| Vendor CSS Task
+|--------------------------------------------------------------------------
+|
+| Take .css files into vendor - Minify them - Push them into name-theme/css
+|
+*/
+
 gulp.task('vendor-css', () => {
   return gulp.src('wp-content/themes/name-theme/vendor/**/*.css')
       .pipe($.concat('vendor.css'))
@@ -53,19 +113,42 @@ gulp.task('vendor-css', () => {
       .pipe(gulp.dest('wp-content/themes/name-theme/css'))
 });
 
-// Task INJECT
+/*
+|--------------------------------------------------------------------------
+| Inject Task
+|--------------------------------------------------------------------------
+|
+| Do bower task, then run vendor-css and vendor-js
+|
+*/
+
 gulp.task('inject', () => {
      runSequence('bower', ['vendor-css', 'vendor-js']);
 });
 
-// Task for FONT files
+/*
+|--------------------------------------------------------------------------
+| Fonts Task
+|--------------------------------------------------------------------------
+|
+| Take Fonts from bower components and push them into name-theme/fonts
+|
+*/
+
 gulp.task('font', () => {
   return gulp.src(['bower_components/**/fonts/*.eot','bower_components/**/fonts/*.woff','bower_components/**/fonts/*.svg','bower_components/**/fonts/*.ttf',])
   .pipe($.flatten())
   .pipe(gulp.dest('wp-content/themes/name-theme/fonts'))
 });
 
-// Task SERVE. Init Browser Sync
+/*
+|--------------------------------------------------------------------------
+| Server Task
+|--------------------------------------------------------------------------
+|
+| Static Server + watching scss/php files
+|
+*/
 gulp.task('serve', function() {
   browserSync.init({
     // Your local server name
